08a27dc208c7a19839f0ded8c0e5ec5e516e3887
philipangeloang <philipangeloang1@gmail.com>
2023-03-01 11:08:52 +0800

    Responsiveness and Modal close

--- frontend/src/components/UpdateForm.js
+++ frontend/src/components/UpdateForm.js
@@ -51,7 +51,7 @@ export default function UpdateForm({ patchID, onPost }) {
 
   return (
     <React.Fragment>
-      <div className="rounded-lg px-6 py-6 font-poppins bg-gray-800 text-white w-1/4 m-auto">
+      <div className="rounded-lg font-poppins p-1 bg-gray-800 text-white w-1/4 m-auto | lg:px-6 lg:py-6">
         <div className="flex flex-col">
           <div className="flex justify-between">
             <h2 className="text-base font-semibold">UPDATE ITEM</h2>
@@ -121,7 +121,12 @@ export default function UpdateForm({ patchID, onPost }) {
                   : "bg-gray-700 p-1 rounded-md"
               }
             />
-            <button className="mt-7 bg-indigo-700 px-1 py-2 rounded-md">
+            <button
+              onClick={() => {
+                onPost(false);
+              }}
+              className="mt-7 bg-indigo-700 px-1 py-2 rounded-md"
+            >
               POST
             </button>
             {error && (
